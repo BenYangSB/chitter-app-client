2d0f848c27e538aa6164ca793b733c4301430c82
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2021-03-26 19:40:47 -0700

    image uploading

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -4,7 +4,7 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import ImageUploader from 'react-images-upload';
 import IMGS from './PostPicture'
-
+import DefaultImg from '../assets/img1.jpeg'
 export default class CreateExercise extends Component {
   constructor(props) {
     super(props);
@@ -22,11 +22,51 @@ export default class CreateExercise extends Component {
       duration: 0,
       date: new Date(),
       users: [],
-      pictures: [] 
+      pictures: [],
+      multerImage: null,
     }
     this.onDrop = this.onDrop.bind(this);
 
   }
+
+  setDefaultImage(uploadType) {
+    if (uploadType === "multer") {
+      this.setState({
+        multerImage: DefaultImg
+      });
+    } 
+  }
+
+    // function to upload image once it has been captured
+  // includes multer and firebase methods
+  uploadImage(e, method) {
+      let imageObj = {};
+    
+      let imageFormObj = new FormData();
+
+      imageFormObj.append("imageName", "multer-image-" + Date.now());
+      imageFormObj.append("imageData", e.target.files[0]);
+
+      // stores a readable instance of 
+      // the image being uploaded using multer
+      this.setState({
+        multerImage: URL.createObjectURL(e.target.files[0])
+      });
+
+      axios.post("http://localhost:5000/image/uploadmulter", imageFormObj)
+        .then((data) => {
+          if (data.data.success) {
+            alert("Image has been successfully uploaded using multer");
+            this.setDefaultImage("multer");
+          }
+        })
+        .catch((err) => {
+          alert("Error while uploading image using multer");
+          this.setDefaultImage("multer");
+        });
+  
+  }
+
 
   setImages(input) {
     this.setState({
@@ -138,9 +178,13 @@ export default class CreateExercise extends Component {
               />
         </div>
 
-        <IMGS
-          setImages = {() => this.setImages}
-        />
+        <div className="process">
+            <h4 className="process__heading">Process: Using Multer</h4>
+            <p className="process__details">Upload image to a node server, connected to a MongoDB database, with the help of multer</p>
+
+            <input type="file" className="process__upload-btn" onChange={(e) => this.uploadImage(e, "multer")} />
+            <img src={this.state.multerImage} alt="upload-image" className="process__image" />
+        </div>
         {
           console.log(this.state.pictures)
         }
