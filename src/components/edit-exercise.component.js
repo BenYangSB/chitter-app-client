1acb7620641fcd7ca428fc1b8826ed03d60237e1
kevin2hang <kevinzhang425@gmail.com>
2021-04-10 22:58:24 -0700

    Made edit recipe use the same component as create recipe (bug in date: when u change date, its not reflected on database for some reason

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -2,22 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import EditRecipeForm from './editRecipeForm';
+import firebase from 'firebase';
 
 export default class EditExercise extends Component {
   constructor(props) {
     super(props);
-
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeIngredients = this.onChangeIngredients.bind(this);
-
-    this.onChangeDuration = this.onChangeDuration.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
     this.state = {
-      username: '',
-      userKey: '',
+      username: firebase.auth().currentUser.displayName,
+      userKey: firebase.auth().currentUser.uid,
+      currUser : this.props.currUser,
       description: '',
       date: '',
       ingredients: '',
@@ -28,131 +22,106 @@ export default class EditExercise extends Component {
     }
   }
 
-  componentDidMount() {
-    axios.get('https://chitterr-app-api.herokuapp.com/exercises/'+this.props.match.params.id)
-      .then(response => {
-        console.log(response.data)
-        this.setState({
-          username: response.data.username,
-          userKey: response.data.userKey,
-          description: response.data.description,
-          duration: response.data.duration,
-          date: response.data.date,
-          instructions: response.data.instructions,
-          ingredients: response.data.ingredients.join(),
-          image: response.data.image
-        })   
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+  // componentDidMount() {
+  //   axios.get('https://chitterr-app-api.herokuapp.com/exercises/'+this.props.match.params.id)
+  //     .then(response => {
+  //       console.log(response.data)
+  //       this.setState({
+  //         username: response.data.username,
+  //         userKey: response.data.userKey,
+  //         description: response.data.description,
+  //         duration: response.data.duration,
+  //         date: response.data.date,
+  //         instructions: response.data.instructions,
+  //         ingredients: response.data.ingredients.join(),
+  //         image: response.data.image
+  //       })   
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
 
-    axios.get('https://chitterr-app-api.herokuapp.com/users/')
-      .then(response => {
-        if (response.data.length > 0) {
-          this.setState({
-            users: response.data.map(user => user.username),
-          })
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+  //   axios.get('https://chitterr-app-api.herokuapp.com/users/')
+  //     .then(response => {
+  //       if (response.data.length > 0) {
+  //         this.setState({
+  //           users: response.data.map(user => user.username),
+  //         })
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     })
 
-  }
+  // }
 
-  onChangeIngredients(e) {
-    this.setState({
-      ingredients: e.target.value
-    })
-  }
+  // onChangeIngredients(e) {
+  //   this.setState({
+  //     ingredients: e.target.value
+  //   })
+  // }
 
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    })
-  }
+  // onChangeUsername(e) {
+  //   this.setState({
+  //     username: e.target.value
+  //   })
+  // }
 
-  onChangeDescription(e) {
-    this.setState({
-      description: e.target.value
-    })
-  }
+  // onChangeDescription(e) {
+  //   this.setState({
+  //     description: e.target.value
+  //   })
+  // }
 
-  onChangeDuration(e) {
-    this.setState({
-      duration: e.target.value
-    })
-  }
+  // onChangeDuration(e) {
+  //   this.setState({
+  //     duration: e.target.value
+  //   })
+  // }
 
-  onChangeDate(date) {
-    this.setState({
-      date: date
-    })
-  }
+  // onChangeDate(date) {
+  //   this.setState({
+  //     date: date
+  //   })
+  // }
 
-  onSubmit(e) {
-    e.preventDefault();
+  // onSubmit(e) {
+  //   e.preventDefault();
 
-    let temp = this.state.ingredients.split(',')
-    const exercise = {
-      username: this.state.username,
-      userKey: this.state.userKey,
-      description: this.state.description,
-      instructions: this.state.instructions,
-      duration: this.state.duration,
-      date: this.state.date,
-      ingredients: temp,
-      image: this.state.image
-    }
+  //   let temp = this.state.ingredients.split(',')
+  //   const exercise = {
+  //     username: this.state.username,
+  //     userKey: this.state.userKey,
+  //     description: this.state.description,
+  //     instructions: this.state.instructions,
+  //     duration: this.state.duration,
+  //     date: this.state.date,
+  //     ingredients: temp,
+  //     image: this.state.image
+  //   }
 
-    console.log(exercise);
+  //   console.log(exercise);
 
-    axios.post('https://chitterr-app-api.herokuapp.com/exercises/update/' + this.props.match.params.id, exercise)
-      .then(res => console.log(res.data));
+  //   axios.post('https://chitterr-app-api.herokuapp.com/exercises/update/' + this.props.match.params.id, exercise)
+  //     .then(res => console.log(res.data));
 
-  }
+  // }
 
   render() {
     return (
     <div class = "absFeed"> 
+      <div>
+          <br></br>
+          <br></br>
+        </div>
       <h3>Edit Recipe</h3>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group"> 
-          Username: {this.state.username}
-        </div>
-        <div className="form-group"> 
-          <label>Description: </label>
-          <input  type="text"
-              required
-              className="form-control"
-              value={this.state.description}
-              onChange={this.onChangeDescription}
-              />
-        </div>
-        <div className="form-group">
-          <label>Duration (in minutes): </label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.duration}
-              onChange={this.onChangeDuration}
-              />
-        </div>
-        <div className="form-group">
-          <label>Ingredients (separated by commas): </label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.ingredients}
-              onChange={this.onChangeIngredients}
-              />
-        </div>
-
-        <div className="form-group">
-          <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
-        </div>
-      </form>
+      <EditRecipeForm 
+        isEdit={true}
+        match={this.props.match}
+        currUser={this.state.currUser}
+        username={this.props.username}
+        userKey={this.props.userKey}
+      />
     </div>
     )
   }
